e5a3630f4854fca57095c27f6c0300371251b401
adithikmah28 <hikmahtiaraditya@gmail.com>
2025-07-26 14:48:46 +0700

    Create custom_data.js

---
+++ custom_data.js
@@ -0,0 +1,25 @@
+// File: custom_data.js
+
+const customData = {
+    // Format: 'ID_TMDB': { videoUrl: '...', synopsis: '...' }
+    
+    // Contoh Movie: Film "Makmum" (ID: 624566)
+    '624566': { 
+        videoUrl: 'https://link-video-makmum-kamu.com/stream.mp4',
+        synopsis: 'Ini adalah sinopsis manual untuk film Makmum. Rini kembali ke asrama tempat ia dulu tinggal atas permintaan Ibu Kinanti yang kesehatannya terus menurun. Namun, kedatangannya justru mengungkap misteri gaib yang menghantui asrama tersebut.' 
+    },
+    
+    // Contoh lain: Film "KKN di Desa Penari" (TMDB ID: 942230)
+    '942230': {
+        videoUrl: 'https://link-kkn-kamu.com/video.html',
+        synopsis: 'Enam mahasiswa yang sedang melakukan KKN di sebuah desa terpencil harus menghadapi teror dari penari misterius yang mengancam keselamatan jiwa mereka.'
+    },
+
+    // Contoh TV Series dengan sinopsis kustom saja
+    '210822': {
+        synopsis: 'Dasiyah, seorang perempuan berbakat, menentang tradisi di industri kretek Indonesia pada tahun 1960-an demi cinta dan kesempurnaan.'
+    },
+
+    // Tambahkan film atau serial lain di sini
+    // 'ID_LAIN': { videoUrl: '...', synopsis: '...' },
+};
